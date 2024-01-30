c7ad9e81078436b7a7a56f92610db1f05f3c4a2e
Fatma Akacha <akachafatma1@gmail.com>
2024-01-30 09:36:48 +0000

    base de donnee

--- server2.js
+++ server2.js
@@ -1,41 +1,90 @@
 const express = require('express');
-const mysql = require('mysql');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const PORT = 3001;
-// Définition de l'en-tête pour permettre l'accès depuis n'importe où
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+const bodyParser = require('body-parser');
+
+app.use(cors()); // Utilisation du middleware cors pour autoriser les requêtes CORS
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3002;
+
 // Configuration de la connexion à la base de données
-const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'test'
-});
-// Connexion à la base de données
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database', err);
-        return;
-    }
-    console.log('Connected to database');
-});
+const db = new sqlite3.Database('./test.db');
+
+// Création de la table si elle n'existe pas
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS etudiants (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    lastname TEXT,
+    firstname TEXT
+  )
+`;
+db.run(createTableQuery);
+
 // Récupération des données de la base de données
 app.get('/etudiants', (req, res) => {
-    const sql = 'SELECT * FROM users';
-    db.query(sql, (error, results) => {
-        if (error) {
-            console.error('Error fetching data', error);
-        } else {
-            res.json(results);
-        }
-    });
+  const sql = 'SELECT * FROM etudiants';
+  db.all(sql, (error, rows) => {
+    if (error) {
+      console.error('Error fetching data', error);
+      res.status(500).send('Error fetching data');
+    } else {
+      res.json(rows);
+    }
+  });
 });
-// Démarrage du serveur
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+// Récupération d'un étudiant par ID
+app.get('/etudiants/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'SELECT * FROM etudiants WHERE id = ?';
+  db.get(sql, [id], (error, row) => {
+    if (error) {
+      console.error('Error fetching data', error);
+      res.status(500).send('Error fetching data');
+    } else {
+      if (!row) {
+        res.json({ lastname: "Etudiant n'existe pas" });
+      } else {
+        res.json(row);
+      }
+    }
+  });
+});
+
+// Mise à jour d'un étudiant par ID
+app.put('/etudiants/:id', (req, res) => {
+  const id = req.params.id;
+  const nom = req.body.lastname;
+  const prenom = req.body.firstname;
+  const sql = 'UPDATE etudiants SET lastname = ?, firstname = ? WHERE id = ?';
+  db.run(sql, [nom, prenom, id], function (err) {
+    if (err) {
+      console.error('Error updating student', err);
+      res.status(500).send('Error updating student');
+    } else {
+      res.send(`Student with ID ${id} updated successfully`);
+    }
+  });
+});
+// ajout 
+app.post('/etudiants', (req, res) => {
+  const nom = req.body.lastname;
+  const prenom = req.body.firstname;
+  const sql = 'INSERT INTO etudiants(lastname,firstname) VALUES (?,?)';
+  db.run(sql, [nom, prenom], function (err) {
+    if (err) {
+      console.error('Error updating student', err);
+      res.status(500).send('Error updating student');
+    } else {
+      res.send(`Student add successfully`);
+    }
+  });
+});
+
+//Démarrage du serveur
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
